de1db20c64171974ab7dc20e6a706461db2d0429
ShoilojoPur <dreamerchachu@gmail.com>
2025-01-20 19:57:37 +0000

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // Model URL from Teachable Machine
 //**************************************************
 //* as before, paste your lnk below
-let URL = "https://teachablemachine.withgoogle.com/models/[...]";
+let URL = "https://teachablemachine.withgoogle.com/models/5nzm1M0G2/";
 
 
 
@@ -223,7 +223,7 @@ async function playInstructionVideo() {
     if (model) {
         processFrame();
     } else {
-        console.log("https://teachablemachine.withgoogle.com/models/[...]");
+        console.log("https://teachablemachine.withgoogle.com/models/5nzm1M0G2/");
     }
 }
 
